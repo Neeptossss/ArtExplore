5c0e06728757f2b996f21aa942529ffddbe5d693
Neeptossss <g.hubler@avis2sante.fr>
2023-01-10 13:16:24 +0100

    init commit

---
+++ frontend/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="w-screen relative flex justify-between items-center p-4">
+    <img class="object-contain h-16 rounded-md" src="@/static/Logo_AA.png"/>
+    <h1 class="text-white pl-4 text-xl font-bold">Musée des Arts Asiatiques de Nice</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+}
+</script>
---
+++ frontend/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-gray-900 w-screen h-screen" className="dark">
+    <Header />
+    <div class="flex h-100 w-100 items-center justify-center">
+      <button class="bg-red-600 rounded-md p-4 text-white font-bold">
+        <i class="bx bxs-compass"></i> Commencez l'aventure !
+      </button>
+    </div>
+    <Tutorial />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+}
+</script>
