b4c2c665360f484412900cd399d31150a92c33cd
Neeptossss <g.hubler@avis2sante.fr>
2023-01-10 15:31:27 +0100

    Adding pins to session code

--- frontend/components/Modal.vue
+++ frontend/components/Modal.vue
@@ -12,10 +12,15 @@
                         <input type="text" name="name" id="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" placeholder="Albert Tartenpion" required>
                     </div>
                     <div>
-                        <label for="session-code" class="block mb-2 text-sm font-medium text-gray-900">Code de session</label>
-                        <input type="password" name="session-code" id="session-code" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5" required>
+                        <label for="name" class="block mb-2 text-sm font-medium text-gray-900">Code de la session</label>
+                        <div class="w-full flex justify-between">
+                        <input ref="pinInputs" type="number" v-model="pin[0]" @keyup.enter="submit" @keyup="nextInput($event, 0)" @input="limitInput" maxlength="1" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-red-500 focus:border-red-500 w-12 h-12 mr-2 text-center" placeholder="_" required>
+                        <input ref="pinInputs" type="number" v-model="pin[1]" @keyup.enter="submit" @keyup="nextInput($event, 1)" @input="limitInput" maxlength="1" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-red-500 focus:border-red-500 w-12 h-12 mr-2 text-center" placeholder="_" required>
+                        <input ref="pinInputs" type="number" v-model="pin[2]" @keyup.enter="submit" @keyup="nextInput($event, 2)" @input="limitInput" maxlength="1" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-red-500 focus:border-red-500 w-12 h-12 mr-2 text-center" placeholder="_" required>
+                        <input ref="pinInputs" type="number" v-model="pin[3]" @keyup.enter="submit" @keyup="nextInput($event, 3)" @input="limitInput" maxlength="1" class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-red-500 focus:border-red-500 w-12 h-12 mr-2 text-center" placeholder="_" required>
+                        </div>
                     </div>
-                    <button type="submit" class="w-full text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Rejoindre la session</button>
+                    <button :class="{'bg-red-800': !allFieldsCompleted }" :disabled="!allFieldsCompleted" @click="submit" class="w-full text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Rejoindre la session</button>
                     <div class="text-sm font-medium text-gray-500">
                         Un problème? <a href="#" class="text-red-600 hover:underline">Centre d'aide</a>
                     </div>
@@ -25,3 +30,48 @@
     </div>
 </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            pin: ['','','',''],
+        }
+    },
+    computed: {
+        allFieldsCompleted() {
+            return this.pin.every((item) => item !== '')
+        },
+    },
+    methods: {
+        nextInput(e, index) {
+            if (e.target.value.length === 1) {
+                if (index !== 3) {
+                    e.target.nextElementSibling.focus()
+                }
+            }
+        },
+        limitInput(e) {
+            if (e.target.value.length > 1) {
+                e.target.value = e.target.value.slice(0, 1)
+            }
+        },
+        submit() {
+            let pin = this.pin.join('')
+            if(pin === '1234'){
+                //valid pin
+                this.$emit('close')
+                // any other actions
+            }else{
+                // invalid pin
+                this.pin = ['','','','']
+                // show an error message or shake the input
+            }
+        }
+    }
+}
+</script>
+
+<style>
+</style>
+
