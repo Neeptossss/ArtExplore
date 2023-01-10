cf0e6fc5ba28bf85ab4e9fa23c75caf939783662
Neeptossss <g.hubler@avis2sante.fr>
2023-01-10 15:41:04 +0100

    Adding scoreboard button + gradient

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,10 +1,13 @@
 <template>
   <div className="dark">
-    <div class="bg-gray-900 w-screen h-screen flex flex-col">
+    <div class="from-gray-800 bg-gradient-to-t to-gray-900 w-screen h-screen flex flex-col">
       <Header />
-      <div class="flex h-full w-full items-center justify-center">
-        <button class="bg-red-600 rounded-md p-4 text-white font-bold" @click="showModal = true">
+      <div class="flex flex-col h-full w-full items-center justify-center gap-4 px-8">
+        <button class="w-full bg-red-600 rounded-md p-4 text-white font-bold" @click="showModal = true">
           <i class="bx bxs-compass"></i> Commencez l'aventure !
+        </button>
+        <button class="w-full border-red-600 border-2 rounded-md p-4 text-white font-bold">
+          <i class="bx bxs-bar-chart-alt-2"></i> Tableau des scores
         </button>
       </div>
       <transition name="fade">
