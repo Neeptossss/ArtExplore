fa0fc6d6c2102a9a0411bcf36a2e52988660a850
Neeptossss <g.hubler@avis2sante.fr>
2023-01-12 13:46:11 +0100

    l

--- backend/controllers/session.controller.js
+++ backend/controllers/session.controller.js
@@ -1,39 +1,147 @@
 const Session = require('../models/session.model');
 
-exports.findPinById = (req, res) => {
+exports.getPinById = (req, res) => {
     Session.getPinById(req.params.id, (err, data) => {
         if (err) {
             if (err.kind === "not_found") {
                 res.status(404).send({
-                    message: `Not found film with id ${req.params.id}.`
+                    message: `No session found with id ${req.params.id}.`
                 });
             } else {
                 res.status(500).send({
-                    message: "Error retrieving film with id " + req.params.id
+                    message: "Error retrieving session with id " + req.params.id
                 });
             }
         } else res.send(data);
     });
 }
 
-exports.findAll = (req, res) => {
+exports.getSessionByPin = (req, res) => {
+    Session.getSessionByPin(req.params.pin, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `No session found with pin ${req.params.pin}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Error retrieving session with id " + req.params.pin
+                });
+            }
+        } else res.send(data);
+    });
+}
+
+exports.getAll = (req, res) => {
     Session.getAll((err, data) => {
         if (err)
             res.status(500).send({
                 message:
-                    err.message || "Some error occurred while retrieving films."
+                    err.message || "Some error occurred while retrieving sessions."
             });
         else res.send(data);
     });
 }
 
-exports.searchFilms = (req, res) => {
-    Session.searchFilms(req.params.search, (err, data) => {
-        if (err)
-            res.status(500).send({
-                message:
-                    err.message || "Some error occurred while retrieving films."
+exports.getCurrentQuestionAndAnswers = (req, res) => {
+    Session.getSessionByPin(req.params.pin, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `No session found with pin ${req.params.pin}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Error retrieving session with id " + req.params.pin
+                });
+            }
+        } else {
+            Session.getEnigmaId(data.step, (err, data) => {
+                if (err) {
+                    if (err.kind === "not_found") {
+                        res.status(404).send({
+                            message: `No enigma found with order ${data.step}.`
+                        });
+                    } else {
+                        res.status(500).send({
+                            message: "Error retrieving enigma with order " + data.step
+                        });
+                    }
+                } else {
+                    Session.getCurrentQuestionAndAnswers(data.id, (err, data) => {
+                        if (err)
+                            res.status(500).send({
+                                message:
+                                    err.message || "Some error occurred while retrieving questions and answers."
+                            });
+                        else res.send(data);
+                    });
+                }
             });
-        else res.send(data);
-    }).then(r => console.log(r));
+        }
+    });
+}
+
+exports.getPastEnigmasQuestionsAndAnswers = (req, res) => {
+    Session.getSessionByPin(req.params.pin, (err, data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `No session found with pin ${req.params.pin}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Error retrieving session with id " + req.params.pin
+                });
+            }
+        } else {
+            Session.getPastEnigmasQuestionsAndAnswers(data.step, (err, data) => {
+                if (err)
+                    res.status(500).send({
+                        message:
+                            err.message || "Some error occurred while retrieving sessions."
+                    });
+                else res.send(data);
+            });
+        }
+    });
+}
+
+exports.validateQuestion = (req, res) => {
+    Session.getQuestionAnswer(req.params.question_id, (err, question_data) => {
+        if (err) {
+            if (err.kind === "not_found") {
+                res.status(404).send({
+                    message: `No question found with id ${req.params.question_id}.`
+                });
+            } else {
+                res.status(500).send({
+                    message: "Error retrieving question with id " + req.params.question_id
+                });
+            }
+        } else {
+            if (question_data.id == req.params.answer_id) {
+                Session.nextStep(req.params.session_id, (err, data) => {
+                    if (err) {
+                        if (err.kind == "not_found") {
+                            res.status(404).send({
+                                message: `No session found with id ${req.params.session_id}.`
+                            });
+                        }
+                        else {
+                            res.status(500).send({
+                                message: "Error updating session with id " + req.params.session_id
+                            });
+                        }
+                    } else {
+                        res.send(data);
+                    }
+                });
+            } else {
+                res.status(418).send({
+                    message: `Wrong answer.`
+                });
+            }
+        }
+    });
 }
--- backend/models/session.model.js
+++ backend/models/session.model.js
@@ -16,7 +16,6 @@ Session.getPinById = (id, result) => {
     if (res.length) {
       debug(res[0])
       result(null, res[0]);
-      
       return;
     }
     result({ kind: "not_found" }, null);
@@ -50,4 +49,69 @@ Session.getAll = result => {
     result(null, res);
   });
 };
-module.exports = Film;
+
+Session.getEnigmaId = (step, result) => {
+  sql.query(`SELECT id FROM enigma WHERE order = ${step}`, (err, res) => {
+    if (err) {
+      debug("error: ", err);
+      result(null, err);
+      return;
+    }
+    debug(res);
+    result(null, res);
+  });
+};
+
+Session.getCurrentQuestionAndAnswers = (enigma, result) => {
+  sql.query(`SELECT * FROM question WHERE id_enigma = ${enigma} AS QUESTION
+  INNER JOIN reply AS REPLY ON QUESTION.id = REPLY.id_question`,
+    (err, res) => {
+      if (err) {
+        debug("error: ", err);
+        result(null, err);
+        return;
+      }
+      debug(res);
+      result(null, res);
+    });
+};
+
+Session.getPastEnigmasQuestionsAndAnswers = (step, result) => {
+  sql.query(`SELECT * FROM enigma WHERE order < ${step} AS ENIGMA
+  INNER JOIN question AS QUESTION ON ENIGMA.id = QUESTION.id_enigma
+  INNER JOIN reply AS REPLY ON QUESTION.id = REPLY.id_question`,
+    (err, res) => {
+      if (err) {
+        debug("error: ", err);
+        result(null, err);
+        return;
+      }
+      debug(res);
+      result(null, res);
+    });
+}
+
+Session.nextStep = (id, result) => {
+  sql.query(`UPDATE session SET step = step + 1 WHERE id = ${id}`, (err, res) => {
+    if (err) {
+      debug("error: ", err);
+      result(null, err);
+      return;
+    }
+    debug(res);
+    result(null, res);
+  });
+}
+
+/* It's getting the answer of the question with the id passed in parameter. */
+Session.getQuestionAnswer = (id, result) => {
+  sql.query(`SELECT * FROM reply WHERE id_question = ${id} AND valid = 1`, (err, res) => {
+    if (err) {
+      debug("error: ", err);
+      result(null, err);
+      return;
+    }
+    debug(res);
+    result(null, res);
+  });
+}
